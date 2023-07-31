ada76ebf0b2317820eab9aee03bf2a30e6180f48
JamieH01 <Jacob__Henderson@outlook.com>
2023-07-31 17:56:57 -0400

    selection!

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Fizzy
 Fizzy is a live fuzzy finder/explorer for directories. Press enter to exit and save a `cd` command corresponding to the last entry to your clipboard to quickly jump to any directory.
+Use the arrow keys to select the path you want. The full command is printed to stdout if you want to use fizzy with a shell script.
 
 ### Installation
 Fizzy can be installed from the crates.io registry:
--- src/main.rs
+++ src/main.rs
@@ -9,38 +9,54 @@ use std::{
     cmp::Ordering,
     fs::{self},
     io::{stdout, Result, Write},
-    rc::Rc, path::PathBuf,
+    rc::Rc, path::PathBuf, env, str::FromStr, process, time::Duration,
 };
 
 use crossterm::{
     event::{read, Event, KeyCode},
     queue,
-    terminal::{disable_raw_mode, enable_raw_mode, Clear, ClearType},
+    terminal::{disable_raw_mode, enable_raw_mode, Clear, ClearType}, execute, cursor,
 };
 
 use defer_lite::defer;
 
+use indicatif::{self, ProgressBar};
+
+
 fn main() -> Result<()> {
-    let paths = collect_paths()?;
+
+    let paths = match collect_paths() {
+        Ok(it) => it,
+        Err(err) => {println!("cannot access {}\r\nerror: {}", get_cwd().to_str().unwrap(), err); process::abort()},
+    };
+    
+    //println!("{paths:?}");
 
     enable_raw_mode()?;
-    defer! {disable_raw_mode().unwrap();};
+    defer! {disable_raw_mode().unwrap(); execute!(stdout(), cursor::Show).unwrap();};
 
     let mut buffer = String::new();
     let mut stdout = stdout();
     let mut targ: String;
+        
+        execute!(stdout, Clear(ClearType::Purge), cursor::Hide)?;
 
+    let mut idx = 0;
     loop {
         queue!(stdout, Clear(ClearType::All))?;
         write!(stdout, "\r\n")?;
 
         if let Some(res) = search(&paths, &buffer) {
+
+            idx = idx.clamp(0, res.len()-1);
+
             unsafe {
-                targ = res.lines().last().unwrap_unchecked().to_owned();
+                targ = res.get_unchecked(idx).clone();
             }
 
-            write!(stdout, "{res}\r\n")
+            write!(stdout, "{}\r\n", join_highlight(res, idx))
         } else {
+            idx = usize::MAX;
             targ = ".".to_owned();
             write!(stdout, "Nobody here but us chickens!\r\n")
         }?;
@@ -52,16 +68,27 @@ fn main() -> Result<()> {
         if let Event::Key(k) = read()? {
             match k.code {
                 KeyCode::Char(c) => {
+                    idx = usize::MAX;
                     buffer.push(c);
                 }
                 KeyCode::Backspace => {
+                    idx = usize::MAX;
                     buffer.pop();
                 }
+
+                KeyCode::Up => {
+                    idx = idx.saturating_sub(1);
+                }
+                KeyCode::Down => {
+                    idx = idx.saturating_add(1);
+                }
+
                 KeyCode::Esc => return Ok(()),
                 KeyCode::Enter => {
                     let mut ctx =
                         copypasta_ext::try_context().expect("failed to get clipboard context");
                     ctx.set_contents(format!("cd {targ}")).unwrap();
+                    println!("cd {targ}");
                     return Ok(());
                 }
 
@@ -79,18 +106,28 @@ fn collect_paths() -> Result<Rc<[Box<str>]>> {
         for entry in fs::read_dir(path)? {
             let entry = entry?;
             let new_path = entry.path().to_string_lossy().into_owned();
+            
 
             if entry.file_type()?.is_dir() && !new_path.contains("/.") {
                 table.push(new_path.clone());
                 collect(PathBuf::from(&new_path), table)?;
+
             }
         }
 
         Ok(())
     }
 
+    let spinner = ProgressBar::new_spinner().with_message("walking directories...");
+    spinner.enable_steady_tick(Duration::from_millis(50)); 
+
     let mut table = vec![];
-    collect(xdg_home::home_dir().unwrap(), &mut table)?;
+
+    let start = get_cwd();
+
+    collect(start, &mut table)?;
+    
+    spinner.is_finished();
 
     //dear god
     let ptr = table
@@ -101,7 +138,33 @@ fn collect_paths() -> Result<Rc<[Box<str>]>> {
     Ok(Rc::from(ptr))
 }
 
-fn search(paths: &Rc<[Box<str>]>, fil: &str) -> Option<String> {
+//possibly optimize by allocating before hand
+fn join_highlight(vec: Vec<String>, idx:usize) -> String {
+    let mut out = String::with_capacity(vec.len()*vec[0].len());
+    for (i, e) in vec.into_iter().enumerate() {
+        if i == idx {
+            out.push_str("\x1b[1;33m");
+            out.push_str(&e);
+            out.push_str("\x1b[0m");
+        } else {
+            out.push_str(&e);
+        }
+        out.push_str("\r\n");
+    }
+
+    out
+}
+
+
+fn get_cwd() -> PathBuf {
+    env::args().nth(1)
+        .map_or_else(
+            || xdg_home::home_dir().unwrap(), 
+            |v| PathBuf::from_str(&v).unwrap()
+            )
+}
+
+fn search(paths: &Rc<[Box<str>]>, fil: &str) -> Option<Vec<String>> {
     if fil.is_empty() {
         return None;
     }
@@ -123,12 +186,12 @@ fn search(paths: &Rc<[Box<str>]>, fil: &str) -> Option<String> {
                 .unwrap_unchecked()
                 .cmp(&a.find(fil).unwrap_unchecked());
             if res == Ordering::Equal {
-                res = b.len().cmp(&a.len())
+                res = b.len().cmp(&a.len());
             }
 
             res
         });
     }
 
-    Some(paths.join("\r\n"))
+    Some(paths.into_iter().map(ToString::to_string).collect())
 }
